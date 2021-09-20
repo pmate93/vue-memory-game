6f29dca44113ed45906b22dec97ebda81c9bc192
Mate Paluska <paluska.mate@gmail.com>
2021-09-20 17:45:36 +0200

    cards responsive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="card-container">
+ <Card v-for="card in cards" :key="card" :cardNumber="card.value" />
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+
+  data(){
+    return{
+      cards: [
+        {id: 0},
+        {value: 0}
+      ],
+    }
+  },
+
+  methods:{
+    generateCards(){
+      this.cards = [];
+      let arr = [];
+
+      while(arr.length < 12){
+        let duplicate = false;
+        let counter = 0;
+        let num = Math.floor(Math.random() * 6);
+        for(let i = 0; i < arr.length;i++){
+          if(arr[i]===num){
+            counter++;
+            if(counter === 2){
+              duplicate = true;
+              break;
+            }
+          }
+        }
+        if(!duplicate){
+          arr.push(num);
+        }
+      }
+      for (let i = 0; i < arr.length; i++) {
+        this.cards.push({
+          value: arr[i],
+          id: i
+        })
+      }
+    }
+  },
+
+  computed:{
+  },
+
+  created(){
+    this.generateCards();
+    console.log(this.cards);
   }
 }
 </script>
@@ -19,8 +70,17 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  
+}
+
+.card-container{
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+<div>{{this.cardNumber}}</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Card',
+  props:{
+      cardNumber: Number
+  },
+  components: {
+
+  },
+
+  data(){
+    return{
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+div{
+    border: 1px solid black;
+    height:2.5rem;
+    width: 5rem;
+    margin:3%;
+    padding:3%;
+    cursor: pointer;
+}
+
+
+</style>
